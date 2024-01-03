dfe5787ba24d68e6530060a19730c4201c265573
Pedro Luzizila <pedro4luzi@yahoo.ca>
2024-01-03 18:32:41 -0500

    enable /admin/ and /admin/doc/

--- DjangoWeb/DjangoWeb/urls.py
+++ DjangoWeb/DjangoWeb/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+from django.conf.urls import include
+admin.autodiscover()
 
 
 urlpatterns = [
@@ -27,4 +29,5 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
 ]
